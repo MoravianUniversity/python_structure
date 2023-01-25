9600117d3d40fb61b28d72deb932d886109f4510
Ben Coleman <colemanb@moravian.edu>
2023-01-25 13:27:12 -0500

    add comments

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 
 from setuptools import setup, find_packages
 
+# Call the setup function with the minium named arguments
 setup(
+    # the name of the library (will be listed with pip list)
     name="python_structure",
+    # the find_packages function will return a list of packages in src
     packages=find_packages('src'),
+    # The empty key stands for the root package
+    # See https://docs.python.org/3/distutils/setupscript.html#listing-whole-packages
     package_dir={'': 'src'}
 )
